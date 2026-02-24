23e15453030571cd42ad138bf89b1be453d99a36
ArzubekDev <arzubekmain@gmail.com>
2026-02-24 18:24:06 +0600

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div className='w-full h-screen flex items-center justify-center bg-indigo-700 text-white/90'>
+            <h1>About Page</h1>
+        </div>
+    );
+};
+
+export default page;
