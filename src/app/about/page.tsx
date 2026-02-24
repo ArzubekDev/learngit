e4cc35dfea4131eb3533c9bac3414485a7f43b7e
ArzubekDev <arzubekmain@gmail.com>
2026-02-24 18:35:12 +0600

    Added content to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const page = () => {
     return (
         <div className='w-full h-screen flex items-center justify-center bg-indigo-700 text-white/90'>
-            <h1>About Page</h1>
+            <h1>About Us</h1>
         </div>
     );
 };
