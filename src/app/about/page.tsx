8c40771c586a184b486cfa0e1534e1bac66c6dd7
ArzubekDev <arzubekmain@gmail.com>
2026-02-24 21:50:36 +0600

    updated

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-indigo-700 text-white/90">
+    <div className="w-full h-screen flex flex-col items-center justify-center bg-indigo-700 text-white/90">
       <h1>About Us</h1>
-      <p>
+      <p className="w-96">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
         consequatur doloribus cum velit porro dolores asperiores quasi aut
         obcaecati molestias at est aliquam dolor alias nihil, cupiditate eius
